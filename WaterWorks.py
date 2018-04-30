4ae3b54fb93b34edd23debdace2fc158c39ee3da
chadilss <chadfinch85@gmail.com>
2018-05-01 00:35:53 +1200

    .

--- WaterWorks.py
+++ WaterWorks.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/home.html')
-def index():
+def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
